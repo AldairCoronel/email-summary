68d1dbfcd7b58733da743ea52abfabaa44e26998
AldairCoronel <aldaircr@ciencias.unam.mx>
2023-03-13 17:40:39 -0600

    refactor: Summary models

--- internal/models/summary.go
+++ internal/models/summary.go
@@ -2,21 +2,16 @@ package models
 
 // This represents month summary
 type MonthSummary struct {
+	Year              int // optional
 	Month             string
 	NumOfTransactions int
 	AverageCredit     float64
 	AverageDebit      float64
 }
 
-type TransactionByMonth struct {
-	Month             string
-	NumOfTransactions int
-}
-
 // This represents the summary information for a set of transaction
 type Summary struct {
 	TotalBalance           float64
 	NumOfTotalTransactions int
-	TransactionByMonth     []TransactionByMonth
 	MonthlySummaries       []MonthSummary
 }
