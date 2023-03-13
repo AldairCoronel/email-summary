0860c588ab9219cd49aceb4021ba6868d1f770b4
AldairCoronel <aldaircr@ciencias.unam.mx>
2023-03-13 17:41:52 -0600

    refactor: Transaction model

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -4,9 +4,7 @@ import "time"
 
 // This represents a debit or credit card transaction on an account
 type Transaction struct {
-	ID        int
-	Date      time.Time
-	Amount    float64
-	IsCredit  bool
-	MonthYear string
+	ID     int
+	Date   time.Time
+	Amount float64
 }
