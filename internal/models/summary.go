fab21ddef1b7d0eeedf65475c4967cf14dccad50
AldairCoronel <aldaircr@ciencias.unam.mx>
2023-03-15 18:43:18 -0600

    update: model summary and monthsummary

--- internal/models/summary.go
+++ internal/models/summary.go
@@ -2,21 +2,25 @@ package models
 
 // This represents the summary information for a set of transaction
 type Summary struct {
-	Id                     int
-	TotalBalance           float64
-	NumOfCreditTansactions int
-	NumOfDebitTansactions  int
-	TotalAverageCredit     float64
-	TotalAverageDebit      float64
+	ID                      int
+	TotalBalance            float64
+	TotalTransactions       int
+	NumOfCreditTransactions int
+	NumOfDebitTransactions  int
+	TotalAverageCredit      float64
+	TotalAverageDebit       float64
+	TransactionID           int
 }
 
 // This represents month summary
 type MonthSummary struct {
-	Id                     int
-	Month                  string
-	NumOfCreditTansactions int
-	NumOfDebitTansactions  int
-	AverageCredit          float64
-	AverageDebit           float64
-	SummaryId              int
+	ID                      int
+	Month                   string
+	TotalBalance            float64
+	TotalTransactions       int
+	NumOfCreditTransactions int
+	NumOfDebitTransactions  int
+	AverageCredit           float64
+	AverageDebit            float64
+	SummaryId               int
 }
