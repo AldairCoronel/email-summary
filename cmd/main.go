9c8b5159f3c8eadca71124f9b5e2cccb682e32b5
AldairCoronel <aldaircr@ciencias.unam.mx>
2023-03-15 18:12:01 -0600

    feat: Load environment variables with godotenv
    This commit adds functionality to the main package to load environment variables using the godotenv package. This makes it easier to manage and configure the application's dependencies without hardcoding values in the source code.

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,26 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/aldaircoronel/email-summary/internal/controller"
 	"github.com/aldaircoronel/email-summary/internal/database"
 	"github.com/aldaircoronel/email-summary/internal/repository"
+	"github.com/aldaircoronel/email-summary/internal/view"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	csvFilePath, _ := filepath.Abs("./sample/txns.csv")
 
-	// Initialize the database connection
-	connStr := "postgres://postgres:hola@localhost:5432/challenge?sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Load database connection string from environment variable
+	connStr := os.Getenv("DATABASE_URL")
 
 	db, err := database.NewPostgresRepository(connStr)
 	if err != nil {
@@ -31,15 +39,32 @@ func main() {
 	if err := ctrl.ProcessCSVFile(context.Background(), csvFilePath); err != nil {
 		log.Fatal(err)
 	}
-
 	// Generate the email summary
-	if err := ctrl.GenerateEmailSummary(context.Background()); err != nil {
+	summary, monthSummaries, err := ctrl.GenerateEmailSummary(context.Background())
+	if err != nil {
 		log.Fatal(err)
 	}
-	// // Send the email summary
-	// if err := view.SendEmail(summary); err != nil {
-	// 	log.Fatal(err)
-	// }
 
-	// fmt.Println("Email summary sent!")
+	// Load email service configuration from environment variables
+	emailCfg := &view.SMTPConfig{
+		Host:     os.Getenv("EMAIL_HOST"),
+		Port:     os.Getenv("EMAIL_PORT"),
+		Username: os.Getenv("EMAIL_USERNAME"),
+		Password: os.Getenv("EMAIL_PASSWORD"),
+		From:     os.Getenv("EMAIL_FROM"),
+	}
+
+	// Initialize email service with SMTP configuration
+	emailService := view.NewSMTPService(emailCfg)
+
+	// Send email summary
+	to := []string{os.Getenv("EMAIL_TO")}
+	subject := "Transaction Summary"
+	body := view.RenderEmailBody(summary, monthSummaries)
+	if err := emailService.SendEmail(to, subject, body); err != nil {
+		log.Fatal(err)
+	}
+
+	// Print message when email is successfully sent
+	log.Println("Email summary sent!")
 }
